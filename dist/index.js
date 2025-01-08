d827833e4ba83c48900da885a2bac84501a6d89b
Akila-I <akila.99g@gmail.com>
2025-01-08 17:30:15 +0530

    Bump JS step version

--- dist/index.js
+++ dist/index.js
@@ -34485,7 +34485,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   // Get all previous steps of the workflow
-  const workflowSteps = github.context;
+  const workflowSteps = JSON.stringify(github.context, undefined, 2)
   console.log(`The steps: ${workflowSteps}`);
 } catch (error) {
   core.setFailed(error.message);
