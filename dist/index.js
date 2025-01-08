e80b4b3b15a2ad0d9ca3f7ec130dada64800836b
Akila-I <akila.99g@gmail.com>
2025-01-08 17:28:49 +0530

    Bump JS step version

--- dist/index.js
+++ dist/index.js
@@ -34485,7 +34485,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   // Get all previous steps of the workflow
-  const workflowSteps = steps;
+  const workflowSteps = github.context;
   console.log(`The steps: ${workflowSteps}`);
 } catch (error) {
   core.setFailed(error.message);
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   // Get all previous steps of the workflow
-  const workflowSteps = steps;
+  const workflowSteps = github.context;
   console.log(`The steps: ${workflowSteps}`);
 } catch (error) {
   core.setFailed(error.message);
